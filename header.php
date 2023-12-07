ad71d1ab0534e406eaaf02d6d3a6bca80ccebd74
optimizationpctwks <86229518+optimizationpctwks@users.noreply.github.com>
2023-12-07 11:22:41 +0300

    Add files via upload

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!-- Fawaz Farooq Ahmed - 2045884 -->
 <!-- Ibrahim Basher Hamid - 2045910 -->
 <ul>
-  <li><a class="active" href="index.php">Home</a></li>
+  <li><a class="active" href="./">Home</a></li>
   <li class="dropdown">
-    <a href="Tools.php">Tools</a>
+    <a href="./Tools.php">Tools</a>
   </li>
-  <li><a href="about.php">About</a></li>
-  <li><a href="JoinUs.php">Join Us</a></li>
-  <li><a href="contact.php">Contact</a></li>
+  <li><a href="./about.php">About</a></li>
+  <li><a href="./JoinUs.php">Join Us</a></li>
+  <li><a href="./contact.php">Contact</a></li>
 </ul>
 
 <style>
